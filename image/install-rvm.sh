1618f3107cac4a83bf52ef141f2dc890d781882b
Stan Cheng <stan@jewlr.com>
2022-06-07 20:45:11 -0400

    Intsall ruby gems 3.0.2; for some reason update latest results in build error

--- image/install-rvm.sh
+++ image/install-rvm.sh
@@ -8,4 +8,4 @@ usermod -a -G rvm deploy
 /sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.7.6 -C --with-jemalloc && rvm use --default 2.7.6"
 # Install ruby 2.6.6 for backwards compatibility until app images have been merged to all branches
 /sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.6.6 -C --with-jemalloc"
-/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system --no-document"
+/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system 3.0.2"
