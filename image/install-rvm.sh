54e27d4889dc9e88c8d4c9e01951d78344071202
Nikhil <nikhil@jewlr.com>
2023-03-24 19:47:14 +0530

    changing rubygems version to 3.3.3 according to build error

--- image/install-rvm.sh
+++ image/install-rvm.sh
@@ -6,7 +6,7 @@
 /sbin/setuser deploy /bin/bash -l -c "curl -sSL https://get.rvm.io | sudo bash -s" # -- --version 1.29.3"
 usermod -a -G rvm deploy
 /sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 3.1.3 -C --with-jemalloc && rvm use --default 3.1.3"
-/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.7.6 -C --with-jemalloc"
+#/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.7.6 -C --with-jemalloc"
 # Install ruby 2.6.6 for backwards compatibility until app images have been merged to all branches
 /sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.6.6 -C --with-jemalloc"
-/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system 3.0.2"
+/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system 3.3.3"
