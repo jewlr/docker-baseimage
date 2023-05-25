9563065c1f6db2b6b2bbc1cd4194188577cace6d
Nikhil <nikhil@jewlr.com>
2023-05-25 11:53:48 -0400

    Added config to update ruby to 3.0.5

--- image/install-rvm.sh
+++ image/install-rvm.sh
@@ -5,7 +5,7 @@
 /sbin/setuser deploy /bin/bash -l -c "curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg2 --import -"
 /sbin/setuser deploy /bin/bash -l -c "curl -sSL https://get.rvm.io | sudo bash -s" # -- --version 1.29.3"
 usermod -a -G rvm deploy
-/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.7.6 -C --with-jemalloc && rvm use --default 2.7.6"
+/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 3.0.5 -C --with-jemalloc && rvm use --default 2.7.6"
 # Install ruby 2.6.6 for backwards compatibility until app images have been merged to all branches
-/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.6.6 -C --with-jemalloc"
-/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system 3.0.2"
+/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.7.6 -C --with-jemalloc"
+/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system 3.2.3"
