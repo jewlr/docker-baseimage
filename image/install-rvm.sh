f4e58aabdbf45eb4364ea673f93093935ffc7438
Stan Cheng <stan@jewlr.com>
2023-07-04 12:03:09 -0400

    Use latest rubygems version: 3.4.15

--- image/install-rvm.sh
+++ image/install-rvm.sh
@@ -6,6 +6,6 @@
 /sbin/setuser deploy /bin/bash -l -c "curl -sSL https://get.rvm.io | sudo bash -s" # -- --version 1.29.3"
 usermod -a -G rvm deploy
 /sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 3.0.5 -C --with-jemalloc && rvm use --default 3.0.5"
-# Install ruby 2.6.6 for backwards compatibility until app images have been merged to all branches
+# Install ruby 2.7.6 for backwards compatibility until app images have been merged to all branches
 /sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && rvm install 2.7.6 -C --with-jemalloc"
-/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system 3.2.3"
+/sbin/setuser deploy /bin/bash -l -c "source /usr/local/rvm/scripts/rvm && gem update --system 3.4.15"
